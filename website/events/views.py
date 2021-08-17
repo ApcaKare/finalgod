9de18c2501668dc8bdc36357ad1e6d63fe60abd8
Charan Sai Dasari <charansaidasari@gmail.com>
2021-08-17 19:38:58 +0530

    ...

--- website/events/views.py
+++ website/events/views.py
@@ -1,8 +1,20 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-
+from rest_framework import viewsets
+from django.shortcuts import render
+from .models import Guntur,EG
+#from api import excel
 def home(request):
 	return render(request, 'events/home.html', {})
 
 def guntur(request):
-	return render(request, 'events/guntur.html', {})
+	#guntur is a variable
+    guntur = Guntur.objects.all()
+    return render(request, 'events/guntur.html',
+        {'guntur': guntur})
+
+def EG(request):
+	#guntur is a variable
+    eg = EG.objects.all()
+    return render(request, 'events/guntur.html',
+        {'EG': eg})
--- website/website/settings.py
+++ website/website/settings.py
@@ -38,6 +38,9 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'events',
+    'rest_framework',
+    'corsheaders',
+    'import_export'
 ]
 
 MIDDLEWARE = [
